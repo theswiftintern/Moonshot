7851dd693c520d3f6bc70747314a3d1f3d30f8da
theswiftintern <theswiftintern@gmail.com>
2022-08-22 16:54:17 +0800

    Day 40, 41 Source: https://www.hackingwithswift.com/100/swiftui/40 - Created structs for astronauts.json and missions.json - Built a generic to load any Codable data - Used ScrollView for list of missions and astronauts, and NavigationLink to navigate as the user goes deeper into details

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by RJ Tedoco on 8/22/22.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
